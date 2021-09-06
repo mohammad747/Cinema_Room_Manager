3b45d4b18bc685ee4950ebd71d9cee447dc8d8f2
Mohammad747 <mohammad.hashemi747@gmail.com>
2021-09-06 13:23:16 +0430

    Errors! add statistics and checking for already purchased seat and wrong input

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -6,19 +6,12 @@ import java.util.Scanner;
 
 public class Cinema {
 
+    private static final double[] stats = new double[4];
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[][] cinemaRoom = initializeTheRoom(scanner);
         menu(scanner, cinemaRoom);
-//        showTheSeats(cinemaRoom);
-//        reserveSeats(cinemaRoom, scanner);
-//        showTheSeats(cinemaRoom);
-
-//        try {
-//            totalIncome(scanner);
-//        } catch (InputMismatchException ime) {
-//            System.out.println("Incorrect input!");
-//        }
     }
 
     private static String[][] initializeTheRoom(Scanner scanner) {
@@ -30,6 +23,7 @@ public class Cinema {
         for (String[] insideArray : cinemaRoom) {
             Arrays.fill(insideArray, "S");
         }
+        totalIncome(rows, seats);
         return cinemaRoom;
     }
 
@@ -41,6 +35,7 @@ public class Cinema {
             System.out.println();
             System.out.println("1. Show the seats\n" +
                     "2. Buy a ticket\n" +
+                    "3. Statistics\n" +
                     "0. Exit");
             menuItem = scanner.nextInt();
             switch (menuItem) {
@@ -50,9 +45,32 @@ public class Cinema {
                 case 2:
                     reserveSeats(cinemaRoom, scanner);
                     break;
+                case 3:
+                    showStatistics();
+                    break;
                 case 0:
                     exit = true;
                     break;
+            }
+        }
+    }
+
+    private static void showStatistics() {
+        System.out.printf("Number of purchased tickets: %d\n", (int) stats[0]);
+        System.out.printf("Percentage:: %.2f%%\n", stats[1]);
+        System.out.printf("Current income: $%d\n", (int) stats[2]);
+        System.out.printf("Total income: $%d\n", (int) stats[3]);
+    }
+
+    private static void totalIncome(int rows, int seats) {
+        int totalSeats = rows * seats;
+        if (totalSeats <= 60) {
+            stats[3] = totalSeats * 10;
+        } else {
+            if (rows % 10 == 0) {
+                stats[3] = rows / 2 * seats * 10 + rows / 2 * seats * 8;
+            } else {
+                stats[3] = rows / 2 * seats * 10 + ((rows / 2) + 1) * seats * 8;
             }
         }
     }
@@ -77,48 +95,67 @@ public class Cinema {
     }
 
     public static String[][] reserveSeats(String[][] cinemaRoom, Scanner scanner) {
+        boolean askAgain = true;
+        int row = 0;
+        int column = 0;
         System.out.println();
-        System.out.println("Enter a row number: ");
-        int row = scanner.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        int column = scanner.nextInt();
-        System.out.println();
-        calculateSeatPrice(cinemaRoom.length, cinemaRoom[0].length, row, column);
+        while (askAgain) {
+            System.out.println("Enter a row number: ");
+            row = scanner.nextInt();
+            System.out.println("Enter a seat number in that row:");
+            column = scanner.nextInt();
+            System.out.println();
+            if (row > cinemaRoom.length || column > cinemaRoom[0].length) {
+                System.out.println("Wrong input!");
+                System.out.println();
+            } else if (cinemaRoom[row - 1][column - 1].equalsIgnoreCase("B")) {
+                System.out.println("That ticket has already been purchased!");
+                System.out.println();
+
+            } else {
+                askAgain = false;
+            }
+        }
+        stats[2] += calculateSeatPrice(cinemaRoom.length, cinemaRoom[0].length, row, column);
+
         for (int i = 0; i < cinemaRoom.length; i++) {
             for (int j = 0; j < cinemaRoom[i].length; j++) {
                 if (i + 1 == row && j + 1 == column) {
                     cinemaRoom[i][j] = "B";
+                    stats[0] += 1;
+                    stats[1] = (stats[0] * 100) / (cinemaRoom.length * cinemaRoom[0].length);
                 }
             }
         }
         return cinemaRoom;
     }
 
-    public static void calculateSeatPrice(int rows, int seats, int selectedRow, int selectedSeat) throws InputMismatchException {
+    public static int calculateSeatPrice(int rows, int seats, int selectedRow, int selectedSeat) throws InputMismatchException {
         if (rows * seats <= 60) {
             System.out.printf("Ticket price: $%d", 10);
             System.out.println();
+            return 10;
         } else {
             if (rows % 2 == 0) {
                 if (selectedRow <= rows / 2) {
                     System.out.printf("Ticket price: $%d", 10);
                     System.out.println();
+                    return 10;
                 } else {
                     System.out.printf("Ticket price: $%d", 8);
                     System.out.println();
+                    return 8;
                 }
-//                System.out.printf("Total income:%n $%d", (rows / 2 * seats * 10) + (rows / 2 * seats * 8));
             } else {
                 if (selectedRow <= rows / 2) {
                     System.out.printf("Ticket price: $%d", 10);
                     System.out.println();
+                    return 10;
                 } else {
                     System.out.printf("Ticket price: $%d", 8);
                     System.out.println();
+                    return 8;
                 }
-//                System.out.printf("Ticket price: $%d", 8);
-//                System.out.println();
-//                System.out.printf("Total income:%n $%d", (rows / 2 * seats * 10) + ((rows / 2 + 1) * seats * 8));
             }
         }
     }
